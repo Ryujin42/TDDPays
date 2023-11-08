345c8157471af6db05cf36a87ff252d874100439
Ryujin42 <ryujin.hector@gmail.com>
2023-11-08 01:53:12 +0100

    feat: finished API

---
+++ TDDPays.Api/Controllers/GovernmentController.cs
@@ -0,0 +1,61 @@
+using ChoETL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TDDPays.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class GovernmentController : ControllerBase
+{
+    private readonly ILogger<GovernmentController> _logger;
+
+    public GovernmentController(ILogger<GovernmentController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetGovernment")]
+    public async Task<IActionResult> GetAsync()
+    {
+        var results = new List<Government>();
+        
+        using (var reader = new ChoCSVReader("government.csv").WithFirstLineHeader().WithDelimiter(","))
+        {
+            foreach (dynamic government in reader)
+            {
+                var result = new Government
+                {
+                    Id = government.Id,
+                    Name = government.Name
+                };
+                results.Add(result);
+            }
+        }
+        
+        if (!results.Any()) return NotFound();
+        return Ok(results);
+    }
+    
+    [HttpGet("{id}", Name = "GetGovernmentById")]
+    public async Task<IActionResult> GetAsyncById(string id)
+    {
+        if (string.IsNullOrWhiteSpace(id)) return BadRequest();
+        
+        using (var reader = new ChoCSVReader("government.csv").WithFirstLineHeader().WithDelimiter(","))
+        {
+            foreach (dynamic government in reader)
+            {
+                if (government.Id == id)
+                {
+                    var result = new Government
+                    {
+                        Id = government.Id,
+                        Name = government.Name
+                    };
+                    return Ok(result);
+                }
+            }
+        }
+        return NotFound();
+    }
+}
---
+++ TDDPays.Api/Government.cs
@@ -0,0 +1,8 @@
+namespace TDDPays.Api;
+
+public class Government
+{
+    public string? Id { get; set; }
+    
+    public string? Name { get; set; }
+}
